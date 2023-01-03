580e9fb8cf7eaa1c092cb6da88b752e4baf3f9e2
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2023-01-03 11:25:19 +0500

    Add initial files and add expense item component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+
+function App() {
+  return (
+    <>
+      <h2>Its First Heading</h2>
+      <ExpenseItem></ExpenseItem>
+    </>
+  );
+}
+
+export default App;
